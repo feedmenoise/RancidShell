cb6292865ee916a94c1239441b3ec846994289e3
feedme <ahmiklha@gmail.com>
2020-11-10 12:46:43 +0200

    old update доделан блэклист пофикшено несколько проблем связаных с запуском в разных окружениях первичный словарь снмп ответов чуть более детальное логгирование

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RancidShell
 Shell for Rancid
+
+Add to crontab from Rancid user
+0 0 * * * /etc/rancid/rancid_updater_v2/run.sh public /etc/rancid/var 
+
+Where: 
+public - snmp ro community
+/etc/rancid/var - directory to rancid reps
--- run.sh
+++ run.sh
@@ -4,116 +4,143 @@
 # 14-05-2019
 
 checkSNMP () {
-#	$1 - ip
+#       $1 - ip
+#echo "checkSNMP runned for $1" >> $logFile
 
-	snmpget -v1 -c $community $1 .1.3.6.1.2.1.1.1.0 | sed "s/SNMPv2-MIB::sysDescr.0 = STRING:/$var/"
+        /bin/snmpget -v1 -c $community $1 .1.3.6.1.2.1.1.1.0 | sed "s/SNMPv2-MIB::sysDescr.0 = STRING: /$var /"
 }
 
 checkExist () {
-#	$1 - ip
-#	$2 - group
+#       $1 - ip
+#       $2 - group
 
-ip=$1";" 
+#echo "checkExist runned for $1 $2" >> $logFile
 
-	for line in $(cat ./dictionary)
-		do
-				group=`echo $line | cut -f 3 -d ';'`
-				if [[ $group != $2 ]]; then
-					if grep -rni --quiet $ip $path/$group/router.db; then
-						sed -i "/$1/d" $path/$group/router.db
-						echo "removed $ip from $group" >> $logFile
-					fi
-				fi
-		done
+ip=$1";"
+
+        for line in $(cat $script_path/dictionary)
+                do
+                        if [ $line = "END" ]; then 
+                                break
+                        else
+                                group=`echo $line | /bin/cut -f 3 -d ';'`
+                                if [[ $group != $2 ]]; then
+                                        if grep -rni --quiet $ip $path/$group/router.db; then
+                                                sed -i "/$1/d" $path/$group/router.db
+                                                echo "UPDATE DB: removed $ip from $group" >> $logFile
+                                        fi
+                                fi
+                        fi
+                done
 }
 
 checkVendor () {
-#	$1 - ip
-#	$2 - expect snmp sanswer
-#	$3 - script
-#	$4 - group name
+#       $1 - ip
+#       $2 - expect snmp sanswer
+#       $3 - script
+#       $4 - group name
+
+#echo "checkVendor runned for $1 $2 $3 $4" >> $logFile
 
 snmpAnswer=$(checkSNMP $1)
 
 db_path="$path/$4/router.db"
 
-	if [[ $snmpAnswer == *"$2"* ]]; then
-		device="$1;$3;up"
+        if [[ $snmpAnswer == *"$2"* ]]; then
+                device="$1;$3;up"
 
-	# grep -rni 172.17.40.10 ./routers/*/router.db
+        # grep -rni 172.17.40.10 ./routers/*/router.db
 
-		if grep --quiet $device $db_path; then
-			echo "$device already in db" >> $logFile
-			checkExist $1 $4
-			res=true
-		else
-			echo $device >> $db_path
-			echo "$device add in $db_path" >> $logFile
-			checkExist $1 $4
-			res=true
-		fi
-	elif [[ $snmpAnswer == "" ]]; then
-		echo "$1 answered on icmp but not answer on snmp" >> $logFile
+                if grep --quiet $device $db_path; then
+                        echo "INFO: $device already in db" >> $logFile
+                        checkExist $1 $4
+                        res=true
+                else
+			mkdir -p "$path/$4"
+                        echo $device >> $db_path
+                        echo "UPDATE DB: $device add in $db_path" >> $logFile
+                        checkExist $1 $4
+                        res=true
+                fi
+        elif [[ $snmpAnswer == "" ]]; then
+                echo "WARNING: $1 answered on icmp but not answer on snmp" >> $logFile
         res=true
-	fi
+        fi
 }
 
 runCheck() {
 # $1 - network
+#echo "runCheck runned for $1" >> $logFile
 
 for var in $(cat $1)
 do
-	res=false
+        res=false
+	
+        for i in $(cat $script_path/dictionary)
+        do
+                if [ $res != true ] && [ $i = "END" ]; then
+                        Answer=$(checkSNMP $var)
+                        echo "WARNING: $Answer not founded in dictionary" >> $logFile
+                        break
+                else
+                        snmp=`echo $i | /bin/cut -f 1 -d';'`
+                        script=`echo $i | /bin/cut -f 2 -d';'`
+                        group=`echo $i | /bin/cut -f 3 -d';'`
 
-	for i in $(cat ./dictionary)
-	do
-		snmp=`echo $i | cut -f 1 -d';'`
-		script=`echo $i | cut -f 2 -d';'`
-		group=`echo $i | cut -f 3 -d';'`
+                        if [[ $res != true ]]; then
+                                checkVendor $var "$snmp" "$script" "$group" 
+		        else
+                                break
+                        fi
+                fi
 
-		if [[ $res != true ]]; then
-			checkVendor $var "$snmp" "$script" "$group"
-		else
-			break
-		fi
+        done
 
-	done	
-
-done	
+done
 
 }
 
 checkAlive() {
+#echo "checkAlive runned for $1 $2 $3" >> $logFile
 # $1 - network
 # $2 - gateway
+# $3 - mask
 
-echo "" > ./networks/$1
-fping -a -g -q -r 1 $1/24 >> ./networks/$1
-sed -i "/$2/d" ./networks/$1
+echo "" > $script_path/networks/$1
+/usr/sbin/fping -a -g -q -r 1 $1/$3 >> $script_path/networks/$1
+sed -i "/$2$/d" $script_path/networks/$1
 
-runCheck ./networks/$1 
+for black in $(cat $script_path/blacklist)
+do
+        sed -i "/$black$/d" $script_path/networks/$1
+        echo "INFO: $black removed from lists" >> $logFile
+done
+
+runCheck $script_path/networks/$1
 }
 
 addNetworks() {
 
-	for net in $(cat ./subnets)
-	do
-		address=`echo $net | cut -f 1 -d ';'`
-		gateway=`echo $net | cut -f 2 -d ';'`
+        for net in $(cat $script_path/subnets)
+        do
+                address=`echo $net | /bin/cut -f 1 -d ';'`
+                gateway=`echo $net | /bin/cut -f 2 -d ';'`
+		mask=`echo $net | /bin/cut -f 3 -d ';'`
+                checkAlive "$address" "$gateway" "$mask" &
 
-		checkAlive "$address" "$gateway" &
-
-	done
+        done
 }
 
 
 IFS=$'\n' #fix wrong cat output
 
 date=$(date +%F_%H-%M-%S)
-logFile=rancidShell.$date
+logFile=/home/rancid/rancidShell.$date
 community=$1
 path=$2
-addNetworks 
+script_path=/etc/rancid/rancid_updater_v2
+addNetworks
 wait
+
 
 
